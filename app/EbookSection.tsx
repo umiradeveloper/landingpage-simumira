a5d8bd96078bd12eea6f45d7fd7e50a423073c40
umiradeveloper <umiradeveloper@gmail.com>
2026-02-13 10:40:09 +0800

    simumira terbaru

--- app/EbookSection.tsx
+++ app/EbookSection.tsx
@@ -6,25 +6,25 @@ export default function EbookSection() {
     <section id="panduan" className="mt-24 px-10 text-white">
       <h3 className="text-4xl text-center">E-book Panduan</h3>
       <h4 className="text-center mt-2 opacity-100 mb-14 ">
-        Klik salah satu E-book untuk mengetahui bagaimana penggunaan CostControl, VMS, dan UmiraDrive.
+        Klik salah satu E-book untuk mengetahui bagaimana penggunaan CostControl, VMS, UmiraDrive, dan Email Corporate.
       </h4>
       
-      <div className="grid md:grid-cols-3 gap-6 max-w-6xl mx-auto h-full mt-10 mb-16">
+      <div className="grid md:grid-cols-2 gap-6 max-w-6xl mx-auto h-full mt-10 mb-16">
         <link rel="stylesheet" href="/" />
         <a href="/pdf/buku-petunjuk-vms.pdf"
         target="_blank">
           <div className=" group
               bg-[#086078] backdrop-blur
-              rounded-xl p-6
+              rounded-2xl p-6
               shadow-lg
               h-full
               transition-all duration-300
               hover:bg-black
               hover:shadow-2xl
-              hover:-translate-y-1
+              hover:-translate-2
               cursor-pointer">
             <BookOpen className="w-6 h-6 text-[#ffffff] transition-transform duration-300 group-hover:scale-110" />
-            <h4 className="mb-2 text-lg mt-2">E-book Penggunaan <span className="font-bold">Vms</span></h4>
+            <h4 className="mb-2 text-lg mt-2">E-book penggunaan <span className="font-bold">Vms</span></h4>
             <p className="text-m opacity-90">
               Tutorial penggunaan VMS disusun untuk memberikan panduan kepada pengguna dalam memahami alur, fitur, dan fungsi utama sistem Vendor Management System.
                Melalui tutorial ini, pengguna diharapkan dapat menggunakan VMS secara efektif, terstruktur, dan sesuai dengan proses yang telah ditetapkan.
@@ -37,15 +37,15 @@ export default function EbookSection() {
         target="_blank">
         <div className=" group
             bg-[#086078] backdrop-blur
-            rounded-xl p-6
+            rounded-2xl p-6
             shadow-lg
             transition-all duration-300
             hover:bg-black
             hover:shadow-2xl
-            hover:-translate-y-1
+            hover:-translate-2
             cursor-pointer">
           <BookOpen className="w-6 h-6 text-[#ffffff] transition-transform duration-300 group-hover:scale-110" /> 
-          <h4 className="mb-2 text-lg mt-2">E-book penggunaan <span className="font-bold">CostControl</span></h4>
+          <h4 className="mb-2 text-lg mt-2">E-book penggunaan <span className="font-bold">Cost Control</span></h4>
           <p className="text-m opacity-90">
           Aplikasi Cost Control merupakan aplikasi yang digunakan untuk membantu pengelolaan biaya proyek agar lebih teratur dan terkontrol. Aplikasi ini dibuat untuk 
           mencatat dan memantau Pendapatan Usaha, 
@@ -57,22 +57,41 @@ export default function EbookSection() {
         target="_blank">
           <div className=" group
               bg-[#086078] backdrop-blur
-              rounded-xl p-6
+              rounded-2xl p-6
               shadow-lg
               h-full
               transition-all duration-300
               hover:bg-black
               hover:shadow-2xl
-              hover:-translate-y-1
+              hover:-translate-2
               cursor-pointer">
             <BookOpen className="w-6 h-6 text-[#ffffff] transition-transform duration-300 group-hover:scale-110" />
-            <h4 className="mb-2 text-lg mt-2">E-book Penggunaan <span className="font-bold">UmiraDrive</span></h4>
+            <h4 className="mb-2 text-lg mt-2">E-book penggunaan <span className="font-bold">Umira Drive</span></h4>
             <p className="text-m opacity-90">
               E-Book Umira Drive adalah panduan praktis yang dirancang untuk membantu pembaca memahami, mengelola, dan memaksimalkan penggunaan Umira Drive secara efektif. 
               Disusun secara sistematis dan mudah dipahami, e-book ini membahas fitur, alur penggunaan, serta tips penting agar pengguna dapat bekerja lebih efisien dan terorganisir.
             </p>
           </div>
         </a>
+          <a href="/pdf/Panduan-Akses-Email-Corporate-Melalui-Browser.pdf"
+          target="_blank">
+            <div className=" group
+                bg-[#086078] backdrop-blur
+                rounded-2xl p-6
+                shadow-lg
+                h-full
+                transition-all duration-300
+                hover:bg-black
+                hover:shadow-2xl
+                hover:-translate-2
+                cursor-pointer">
+              <BookOpen className="w-6 h-6 text-[#ffffff] transition-transform duration-300 group-hover:scale-110" />
+              <h4 className="mb-2 text-lg mt-2">E-book akses <span className="font-bold">Email Corporate</span></h4>
+              <p className="text-m opacity-90">
+              E-book Email Corporate disusun sebagai panduan resmi untuk membantu pengguna memahami prosedur aktivasi, akses, dan pengelolaan email dengan domain perusahaan. Melalui panduan ini, pengguna diharapkan dapat menggunakan email corporate secara profesional, aman, dan sesuai dengan standar komunikasi yang berlaku, sehingga mendukung kredibilitas serta kelancaran operasional perusahaan.
+              </p>
+            </div>
+          </a>
       </div>
     </section>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-linear-to-b from-[#FBF6C9] via-[#9FB4B8] to-[#1F4F8B]">
       {/* Header */}
-      <header className="flex items-center justify-between px-10 py-6">
+      <header className="flex items-center justify-between px-8 py-4">
          <Link href="/" className="flex items-center">
           <Image
             src="/images/umiralogo.png"
@@ -19,22 +19,22 @@ export default function Home() {
         </Link>
         <Link 
         href="#panduan"
-        className=" text-2xl text-[#086078] mr-5">
+        className=" text-xl text-[#086078] mr-10">
           Panduan
         </Link>
       </header>
 
       {/* Hero */}
-      <section className="text-center mt-20">
-        <h2 className="text-7xl tracking-thight font-bold justify-center text-[#086078]">
-          Welcome to Umira
+      <section className="text-center mt-27">
+        <h2 className="text-7xl tracking-thight font-bold justify-center text-[#086078] ">
+          Welcome to SimUmira
         </h2>
         <p className="mt-1 text-[#086078] max-w-lg mx-auto text-xl leading-thight justify-center ">
           professional, rapidly growing, sustainable design and engineering
-          construction company since 2016.
+          construction company since 2016.  
         </p>
 
-        <div className="flex flex-col items-center md:flex-row md:justify-center gap-6 mt-10">
+        <div className="flex flex-col items-center md:flex-row md:justify-center gap-5 mt-10 mb-30">
           <a href="https://superapps.simumira.com/apps/LoginRegister/"
           target="_blank">
           <button className="group
@@ -98,26 +98,112 @@ export default function Home() {
             
            </button>     
           </a>
+          <a href="https://webmail.umiraglobal.com/"
+          target="_blank">
+          <button className="group
+            px-10 py-2.5
+            rounded-full
+            bg-[#086078]
+            text-white
+            font-semibold
+            flex items-center gap-2
+            transition-all duration-300
+            hover:bg-[#164048]
+            hover:shadow-lg
+            hover:-translate-y-0.5
+            ">
+            Email Corporate
+            <span className="transition-transform duration-300 group-hover:translate-x-1">
+                →
+            </span>
+            
+           </button>     
+          </a>
         </div>
       </section>
       
-      <div className="grid place-items-center">    
-      <Link href="https://vms.simumira.com/"
+        <div className="grid md:grid-cols-3 gap-2 place-items-center">    
+          {/* foto 1 vms */}
+        <Link href="https://vms.simumira.com/"
+        target="_blank">
+        <div className="vms-card group relative py-1 p-1 mt-20 w-full max-w-md cursor-pointer overflow-hidden shadow-lg transition-all duration-300 hover:shadow-2xl  rounded-4xl ">
+          <Image 
+            src="/images/Vms-login.png" 
+            alt="VMS Login"
+            width={700}
+            height={400}
+            className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-120 rounded-4xl"
+          />
+
+          <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-400 group-hover:opacity-100" />
+
+          <div className="absolute bottom-4 left-4 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+            <h3 className="text-xl font-semibold">VMS</h3>
+            <p className="text-sm">Klik untuk membuka Vms Login</p>
+          </div>
+        </div>
+      </Link>
+    {/* foto 2 SuperApps */}
+      <Link href="https://superapps.simumira.com/apps/LoginRegister/"
       target="_blank">
-      <div className="vms-card group relative py-5 p-1 mt-20 w-full max-w-md cursor-pointer overflow-hidden shadow-lg transition-all duration-300 hover:shadow-2xl">
+      <div className="vms-card group relative py-1 p-1 mt-20 w-full max-w-md cursor-pointer overflow-hidden shadow-lg transition-all duration-300 hover:shadow-2xl rounded-4xl ">
         <Image 
-          src="/images/vms-login.png" // ganti sesuai gambar kamu
-          alt="VMS login"
+          src="/images/SuperApps.png" 
+          alt="SuperApps Login"
           width={700}
           height={400}
-          className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
+          className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-120 rounded-4xl"
         />
 
         <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
 
         <div className="absolute bottom-4 left-4 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
-          <h3 className="text-xl font-semibold">VMS Login</h3>
-          <p className="text-sm">Klik untuk membuka Vms Login</p>
+          <h3 className="text-xl font-semibold">SuperApps</h3>
+          <p className="text-sm">Klik untuk membuka SuperApps Login</p>
+        </div>
+      </div>
+    </Link>
+    {/* foto 3 webmail */}
+      <Link href="https://webmail.umiraglobal.com/"
+      target="_blank">
+      <div className="vms-card group relative py-1 p-1 mt-20 w-full max-w-md cursor-pointer overflow-hidden shadow-lg transition-all duration-300 hover:shadow-2xl rounded-4xl ">
+        <Image 
+          src="/images/webmail.png" 
+          alt="Webmail"
+          width={700}
+          height={400}
+          className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-120 rounded-4xl"
+        />
+
+        <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+
+        <div className="absolute bottom-4 left-4 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+          <h3 className="text-xl font-semibold">Email Corporate</h3>
+          <p className="text-sm">Klik untuk membuka Email Corporate Login</p>
+        </div>
+      </div>
+    </Link>
+
+    {/* foto 4 UmiraDrive */}
+    
+    </div>
+    <div className="flex justify-center">
+       <Link href="https://drive.simumira.com/#/signin"
+      target="_blank">
+      <div className="vms-card group relative py-1 p-1 mt-10 w-full max-w-md cursor-pointer overflow-hidden shadow-lg transition-all duration-300 hover:shadow-2xl justify-center rounded-4xl ">
+        <Image 
+          src="/images/Umira_Drive.png"
+          alt="Umira drive"
+          width={700}
+          height={400}
+          className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-120 rounded-4xl"
+        />
+
+        <div className="absolute inset-0 bg-black/40 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+
+        <div className="absolute bottom-4 left-4 text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+          <h3 className="text-xl font-semibold">UmiraDrive</h3>
+          <p className="text-sm">Klik untuk membuka UmiraDrive Login</p>
         </div>
       </div>
     </Link>
@@ -131,7 +217,7 @@ export default function Home() {
       <EbookSection />
 
       {/* Footer */}
-      <footer className="text-center text-white text-sm py-10 mb-0">
+      <footer className="text-center text-white text-sm py-3 mb-0">
         © Umira Sinergi Global 2026. All rights reserved.
       </footer>
     </main>
